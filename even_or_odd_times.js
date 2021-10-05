93999173823a413b197be6770401765e6331e745
Bruhenor <horvath.aron1990b@gmail.com>
2021-10-05 19:33:43 +0200

    Added_Simple multiplication_kata_8

---
+++ even_or_odd_times.js
@@ -0,0 +1,19 @@
+
+
+const number = 34
+
+let res 
+
+const asd = (number % 2 == 0)? number*8 : number * 9 
+
+console.log(asd);
+
+    if(number % 2 == 0) {
+     res = number * 8;
+}
+
+else {
+    res = number * 9;
+}
+
+console.log(res);
