c83e497a791aa23ae2dfa9d2cb0eb27fdb3ede3c
Bruhenor <horvath.aron1990b@gmail.com>
2021-10-05 20:12:05 +0200

    fixed even or odd format

--- even_or_odd_times.js
+++ even_or_odd_times.js
@@ -1,19 +1,15 @@
+// const asd = (number % 2 == 0)? number*8 : number * 9
 
-
-const number = 34
-
-let res 
-
-const asd = (number % 2 == 0)? number*8 : number * 9 
-
-console.log(asd);
-
-    if(number % 2 == 0) {
-     res = number * 8;
-}
-
-else {
+function evenOrOdd(number) {
+  let res;
+  if (number % 2 == 0) {
+    res = number * 8;
+  } else {
     res = number * 9;
+  }
+  return res;
 }
 
-console.log(res);
+const test1 = evenOrOdd(22);
+
+console.log(test1);
