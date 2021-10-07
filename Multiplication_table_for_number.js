1d8d19b98afcd123b17bf8df6ac1a651547a171f
Bruhenor <horvath.aron1990b@gmail.com>
2021-10-07 21:01:44 +0200

    Multi_table

---
+++ Multiplication_table_for_number.js
@@ -0,0 +1,17 @@
+
+
+function multiTable(number){
+
+    let result 
+
+    for (let i= 0; i<=10; i++) {
+        result += '${i} * ${number} = $(i * number)'
+        if (i<10){
+            result += "\n"
+        }
+        
+    }
+
+    return result
+
+}
